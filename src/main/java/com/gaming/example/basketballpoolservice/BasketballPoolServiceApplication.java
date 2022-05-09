47e319d93bd85ca2063aa0995fad5309f5d6ce9c
Bruce LeBlanc <bruce.leblanc.westwood@gmail.com>
2022-05-09 19:58:13 -0300

    use spring.io generated project

---
+++ src/main/java/com/gaming/example/basketballpoolservice/BasketballPoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gaming.example.basketballpoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballPoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballPoolServiceApplication.class, args);
+	}
+
+}
